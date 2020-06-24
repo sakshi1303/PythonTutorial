e4f7fa29045c45060133a04b33c67165cd2efc17
Sakshi Agarwal <agarwalsakshi.13@gmail.com>
2020-06-24 20:57:28 +0530

    variable main

---
+++ area.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 24 20:47:18 2020
+
+@author: Sakshi Agarwal
+"""
+
+def cal_area(base, height):
+    return(base * height)
+
+
+print("From area:", __name__)
+
+if __name__ == "__main__":
+    res  = cal_area(2,3)
+    print(res)
+
+
---
+++ caller.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 24 20:50:36 2020
+
+@author: Sakshi Agarwal
+"""
+import area
+print("from caller:",__name__)
+print(area.cal_area(3,4))
